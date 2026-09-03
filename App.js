1241636d206258588f5350b507f09f6e6dba85d8
Gitoldor <rajguruowais@gmail.com>
2026-09-03 23:50:56 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,15 @@
+const posts = [
+  {
+    id: 1,
+    title: "Welcome",
+    topic: "Scenic Drives",
+    type: "",
+    subtype: "",
+    lat: 15.9041,
+    lng: 73.8219,
+    image: "https://i.ibb.co/chqGP3bQ/SAWANT-WADI-20260831-173946-0000.png",
+    link: "welcome.html",
+    plan: "FREE",
+    createdAt: "2026-09-03"
+  }
+];
