77c223df39e875e55c511a6474355b50c10da220
Gitoldor <rajguruowais@gmail.com>
2026-09-03 23:57:51 +0530

    Add post 2

--- App.js
+++ App.js
@@ -11,5 +11,18 @@ const posts = [
     link: "welcome.html",
     plan: "FREE",
     createdAt: "2026-09-03"
+  },
+  {
+    id: 2,
+    title: "Yo",
+    topic: "Yo",
+    type: "",
+    subtype: "",
+    lat: 3,
+    lng: 3,
+    image: "https://api.github.com/repos/",
+    link: "https://api.github.com/repos/",
+    plan: "FREE",
+    createdAt: "2026-09-03"
   }
 ];
